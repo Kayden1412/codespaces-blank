723bff51994e93acf18a76e32f220732944cc02e
Kayden1412 <143221653+Kayden1412@users.noreply.github.com>
2023-12-03 04:57:18 +0700

    Update +page.server.js

--- my-app/src/routes/anime/+page.server.js
+++ my-app/src/routes/anime/+page.server.js
@@ -2,7 +2,7 @@ export const csr = false;
 
 export async function load() {
     
-    const anime = await fetch("https://api.jikan.moe/v4/seasons/now?limit=50")
+    const anime = await fetch("https://api.jikan.moe/v4/seasons/now?page=2")
     const response = await anime.json()
     return { anime: response}
 }
