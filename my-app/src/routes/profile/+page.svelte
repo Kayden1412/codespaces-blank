89652ce6915e1ca658b05e43edfc43a0103d04d3
Kayden1412 <143221653+Kayden1412@users.noreply.github.com>
2023-12-10 16:31:14 +0000

    name

--- my-app/src/routes/profile/+page.svelte
+++ my-app/src/routes/profile/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 
     import { page } from '$app/stores';
+    console.log(page.session)
 </script>
 
 
 <img src={$page.session.user.image} alt="Avatar image">
+<h4 class="text-primary-600">{$page.session.user.name}</h4>
